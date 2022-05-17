016a8c6b831e603d4e98436f3b06e5b7520cd0ef
varunnaik1121 <varunnaik1121@gmail.com>
2022-05-18 00:46:03 +0530

    first-commit

--- pages/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-export const getServerSideProps = async () => {
-  const query = '*[_type=="product"]';
-  const products = await client.fetch(query);
-  const bannerquery = '*[_type=="banner"]';
-  const bannerData = await client.fetch(bannerquery);
-  return {
-    props: { products, bannerData },
-  };
-};
